5a7aab868f9968af956019ae5791e6cce0eea064
DarkCaster <dark.caster@outlook.com>
2016-07-10 13:48:55 +0300

    Added bash script for build optional dependencies

---
+++ build-optional-deps.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+script_dir="$( cd "$( dirname "$0" )" && pwd )"
+
+check_error () {
+if [ "$?" != "0" ]; then
+	echo "Build failed"
+	exit 1
+fi
+}
+
+"$script_dir/External/DotNetBuildTools/prepare-and-build.sh" "$script_dir/External"
+check_error
+
+cp "$script_dir/External/DotNetBuildTools/dist/extra/NuGet.Config" "$script_dir/NuGet.Config"
+check_error
+
+chmod 644 "$script_dir/NuGet.Config"
+check_error
+
