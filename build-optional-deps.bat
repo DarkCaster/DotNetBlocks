d13a70ef16340270e3eb70a0a343f47e81827a50
DarkCaster <dark.caster@outlook.com>
2016-07-11 16:02:22 +0300

    build-optional-deps.bat - use "call" to start external bat file, change ScriptRoot variable to random name, so child script will not spoil it

--- build-optional-deps.bat
+++ build-optional-deps.bat
@@ -1,9 +1,9 @@
 @echo off
-set ScriptRoot=%~dp0
+REM Use random variable name, so calling another bat file will not spoil it.
+set aafc2aac361f=%~dp0
 
-"%ScriptRoot%\External\DotNetBuildTools\prepare-and-build.bat" "%ScriptRoot%\External"
+call "%aafc2aac361f%\External\DotNetBuildTools\prepare-and-build.bat" "%aafc2aac361f%\External"
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
 
-copy "%ScriptRoot%\External\DotNetBuildTools\dist\extra\NuGet.Config" "%ScriptRoot%\NuGet.Config"
+copy /Y "%aafc2aac361f%\External\DotNetBuildTools\dist\extra\NuGet.Config" "%aafc2aac361f%\NuGet.Config"
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
-
