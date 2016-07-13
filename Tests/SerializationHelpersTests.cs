36e5272e7ded0a2e23a204749c31e29f80601806
DarkCaster <dark.caster@outlook.com>
2016-07-13 19:29:33 +0300

    Added unified serialization helper test - BadDataDeserialize

--- Tests/SerializationHelpersTests.cs
+++ Tests/SerializationHelpersTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using DarkCaster.Serialization;
 using NUnit.Framework;
 
@@ -46,6 +47,21 @@ namespace Tests
 			Assert.AreEqual(data1,data2);
 		}
 		
+		public static void BadDataDeserialize<TC,SER1,SER2>(Type expectedException, SER1 genSer, SER2 objSer)
+			where SER1 : ISerializationHelper<TC>
+			where SER2 : ISerializationHelper
+		{
+			var random=new Random();
+			var data=new byte[random.Next(512,8192)];
+			random.NextBytes(data);
+			Assert.Throws(expectedException,()=>genSer.Deserialize(data));
+			Assert.Throws(expectedException,()=>objSer.DeserializeObj(data));
+			var encoding = Encoding.GetEncoding("us-ascii", new EncoderReplacementFallback(" "), new DecoderReplacementFallback(" "));
+			var badString=encoding.GetString(data);
+			Assert.Throws(expectedException,()=>genSer.Deserialize(badString));
+			Assert.Throws(expectedException,()=>objSer.DeserializeObj(badString));
+		}
+		
 		public static void WrongTypeSerialize<SER>(Type expectedException, object wrongObject, SER serializer)
 			where SER : ISerializationHelper
 		{
@@ -53,7 +69,7 @@ namespace Tests
 			Assert.Throws(expectedException,()=>serializer.SerializeObjToString(wrongObject));
 		}
 		
-		//Some serializers (like json) actually CAN deserialize and apply data to wrong object type,
+		//Some serializers (like json) actually CAN deserialize and apply data from different object type,
 		//so this check may be skipped in such cases
 		public static void WrongTypeDeserialize<SER>(Type expectedException, object obj, SER correctSerializer, SER wrongSerializer)
 			where SER : ISerializationHelper
