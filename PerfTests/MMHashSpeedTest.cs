b45a70ff04d8e6e4a3abae509ce3729b64e700bc
DarkCaster <dark.caster@outlook.com>
2016-07-15 01:33:20 +0300

    MMHashSpeedTest - count also TotalProcessorTime

--- PerfTests/MMHashSpeedTest.cs
+++ PerfTests/MMHashSpeedTest.cs
@@ -15,18 +15,21 @@ namespace PerfTests
 			var seed=unchecked((uint)random.Next(int.MinValue,int.MaxValue));
 			var data=new byte[1024*1024+3];
 			random.NextBytes(data);
-			var mmHash=new MMHash32(seed);
-			
+			var mmHash=new MMHash32(seed);	
 			var sw=new Stopwatch();
-			
+			var process=Process.GetCurrentProcess();	
+			var startCpuTime=process.TotalProcessorTime;
 			sw.Start();
 			for(int i=0;i<iter;++i)
 				hash=mmHash.GetHash(data);
 			sw.Stop();
+			var stopCpuTime=process.TotalProcessorTime;
+			var cpuDiff=(stopCpuTime-startCpuTime).TotalSeconds;
 			var diff=sw.Elapsed.TotalSeconds;
 			var totalSize=data.Length*iter;
 			var speed=(totalSize/(1024*1024))/diff;
 			Console.WriteLine(string.Format("MMHash32.GetHash: {0} B in {1:##.##} S. Speed=={2:##.###} Mb/S.",totalSize,diff,speed));
+			Console.WriteLine(string.Format("MMHash32.GetHash: total processor time {0:##.###} S",cpuDiff));
 		}
 	}
 }
