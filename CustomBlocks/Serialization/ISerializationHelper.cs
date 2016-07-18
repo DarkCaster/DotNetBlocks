ab1629d871a3f1d79fec41ea75d7bc3479cd4389
DarkCaster <dark.caster@outlook.com>
2016-07-18 22:31:03 +0300

    Updated ISerializationHelper interface - added new methods and some basic usage docs

--- CustomBlocks/Serialization/ISerializationHelper.cs
+++ CustomBlocks/Serialization/ISerializationHelper.cs
@@ -31,9 +31,45 @@ namespace DarkCaster.Serialization
 	/// </summary>
 	public interface ISerializationHelper<T>
 	{
+		/// <summary>
+		/// Serialize the specified target object. Returns serialized data as byte array.
+		/// </summary>
+		/// <returns>Returns serialized data as byte array.</returns>
+		/// <param name="target">Target object</param>
 		byte[] Serialize(T target);
-		T Deserialize(byte[] data);
+
+		/// <summary>
+		/// Serialize the specified target object. Returns serialized data as string.
+		/// Recomennded use - text transfer protocols and config files.
+		/// </summary>
+		/// <returns>Returns serialized data as string.</returns>
+		/// <param name="target">Target object</param>
 		string SerializeToString(T target);
+
+		/// <summary>
+		/// Serialize the specified target object. Insert serialized data into destination array at specified offset.
+		/// Returns length of serialized data written.
+		/// </summary>
+		/// <returns>Returns length of serialized data written</returns>
+		/// <param name="target">Target object</param>
+		/// <param name="dest">Destination array where serialized data will be inserted. Array must have enough free size</param>
+		/// <param name="offset">Offset where to start inserting data</param>
+		int Serialize(T target, byte[] dest, int offset = 0);
+
+		/// <summary>
+		/// Deserialize the specified data array to new object
+		/// </summary>
+		/// <returns>New object, deserialized from byte array</returns>
+		/// <param name="data">byte array with serialized data</param>
+		/// <param name="offset">optional offset</param>
+		/// <param name="len">optional length</param>
+		T Deserialize(byte[] data, int offset = 0, int len = 0);
+
+		/// <summary>
+		/// Deserialize the specified string to new object
+		/// </summary>
+		/// <returns>New object, deserialized from string</returns>
+		/// <param name="data">string with serialized data</param>
 		T Deserialize(string data);
 	}
 
@@ -43,9 +79,45 @@ namespace DarkCaster.Serialization
 	/// </summary>
 	public interface ISerializationHelper
 	{
+		/// <summary>
+		/// Serialize the specified target object. Returns serialized data as byte array.
+		/// </summary>
+		/// <returns>Returns serialized data as byte array.</returns>
+		/// <param name="target">Target object</param>
 		byte[] SerializeObj(object target);
-		object DeserializeObj(byte[] data);
+
+		/// <summary>
+		/// Serialize the specified target object. Returns serialized data as string.
+		/// Recomennded use - text transfer protocols and config files.
+		/// </summary>
+		/// <returns>Returns serialized data as string.</returns>
+		/// <param name="target">Target object</param>
 		string SerializeObjToString(object target);
+
+		/// <summary>
+		/// Serialize the specified target object. Insert serialized data into destination array at specified offset.
+		/// Returns length of serialized data written.
+		/// </summary>
+		/// <returns>Returns length of serialized data written</returns>
+		/// <param name="target">Target object</param>
+		/// <param name="dest">Destination array where serialized data will be inserted. Array must have enough free size</param>
+		/// <param name="offset">Offset where to start inserting data</param>
+		int SerializeObj(object target, byte[] dest, int offset = 0);
+
+		/// <summary>
+		/// Deserialize the specified data array to new object
+		/// </summary>
+		/// <returns>New object, deserialized from byte array</returns>
+		/// <param name="data">byte array with serialized data</param>
+		/// <param name="offset">optional offset</param>
+		/// <param name="len">optional length</param>
+		object DeserializeObj(byte[] data, int offset = 0, int len = 0);
+
+		/// <summary>
+		/// Deserialize the specified string to new object
+		/// </summary>
+		/// <returns>New object, deserialized from string</returns>
+		/// <param name="data">string with serialized data</param>
 		object DeserializeObj(string data);
 	}
 }
