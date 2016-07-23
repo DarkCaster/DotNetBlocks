55367a72cd920c224990fe1a769c2b8abdc22d19
DarkCaster <dark.caster@outlook.com>
2016-07-23 04:16:59 +0300

    Added abstracts for furure 'SerializationFactory classes

---
+++ CustomBlocks/Serialization/SerializationFactoryException.cs
@@ -0,0 +1,43 @@
+﻿// SerializationFactoryException.cs
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2016 DarkCaster <dark.caster@outlook.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+using System;
+
+namespace DarkCaster.Serialization
+{
+	/// <summary>
+	/// Base exception for serialization factories classes
+	/// </summary>
+	public abstract class SerializationFactoryException : Exception
+	{
+		public readonly Type factoryType;
+		public readonly Type requestedHelperType;
+		protected SerializationFactoryException(Type factoryType, Type requestedHelperType, string message, Exception inner) : base(message, inner)
+		{
+			this.factoryType = factoryType;
+			this.requestedHelperType = requestedHelperType;
+		}
+	}
+}
