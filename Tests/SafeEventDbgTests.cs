c23a1e14b61f0ea4ff3e6ebc0aeabe7df96735e4
DarkCaster <dark.caster@outlook.com>
2016-08-26 01:49:45 +0300

    SafeEvent - added stale-object test for SafeEventDbg class

--- Tests/SafeEventDbgTests.cs
+++ Tests/SafeEventDbgTests.cs
@@ -63,5 +63,54 @@ namespace Tests
 			var pub = new SimplePublisher<SafeEventDbg<TestEventArgs>, SafeEventDbg<TestEventArgs>>(ev, ev);
 			CommonEventTests.Raise(sub1, pub);
 		}
+
+		private class StaleSubscriber
+		{
+			public const int PLACEHOLDERLEN = 32768;
+			private readonly byte[] placeHolder;
+			~StaleSubscriber() { finalizerDone = true; }
+			public StaleSubscriber()
+			{
+				placeHolder = new byte[PLACEHOLDERLEN];
+				for(int i = 0; i < PLACEHOLDERLEN; ++i)
+					placeHolder[0] =(byte)(i % 255);
+			}
+			public void OnTestEvent(object sender, TestEventArgs args) { throw new NotSupportedException("This method should never been run!"); }
+		}
+
+		private static volatile bool finalizerDone = false;
+
+		[Test]
+		public void StallObjectDetect()
+		{
+			finalizerDone = false;
+			var ev = new SafeEventDbg<TestEventArgs>();
+			var pub = new SimplePublisher<SafeEventDbg<TestEventArgs>, SafeEventDbg<TestEventArgs>>(ev, ev);
+			long cnt = 0L;
+			const int MAXITER = 5000;
+			while(!finalizerDone)
+			{
+				pub.TheEvent.Subscribe((new StaleSubscriber()).OnTestEvent);
+				GC.Collect();
+				GC.WaitForPendingFinalizers();
+				++cnt;
+				if(cnt > MAXITER)
+					Assert.Inconclusive(string.Format("GC was expected, but, still, has not been run yet, cannot proceed this test. " +
+					                                  "GC routine is not working as expected in your .NET runtime! " +
+					                                  "At least {0} MiB of memory was consumed during this test while waiting GC to proceed.",
+					                                  (StaleSubscriber.PLACEHOLDERLEN*MAXITER)/(1024*1024)));
+			}
+			try { pub.Raise(); }
+			catch(Exception ex)
+			{
+				Assert.AreEqual(typeof(EventDbgException), ex.GetType());
+				var dbgEx = (EventDbgException)ex;
+				Assert.Null(dbgEx.subscriber);
+				Assert.NotNull(dbgEx.declaringType);
+				Assert.AreEqual(typeof(StaleSubscriber), dbgEx.declaringType);
+				Assert.Pass();
+			}
+			Assert.Fail("Expected EventDbgException exception has not been triggered during event raise, test failed!");
+		}
 	}
 }
