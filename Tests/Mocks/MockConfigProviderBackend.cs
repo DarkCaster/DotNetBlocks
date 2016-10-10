e70adfdf4e0f35e8b122be7519b88acf0f924fbe
DarkCaster <dark.caster@outlook.com>
2016-10-10 18:24:25 +0300

    FileConfigProvider - added mock for storage backend

---
+++ Tests/Mocks/MockConfigProviderBackend.cs
@@ -0,0 +1,57 @@
+﻿// MockConfigProviderBackend.cs
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2016 DarkCaster <dark.caster@outlook.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+using System;
+using System.Threading.Tasks;
+using DarkCaster.Config.Private;
+
+namespace Tests.Mocks
+{
+	/// <summary>
+	///  Storage backend mock for use with tests of FileConfigProvider
+	/// </summary>
+	public class MockConfigProviderBackend : IConfigStorageBackend
+	{
+		public StorageBackendInitResponse Init(object initData)
+		{
+			throw new NotImplementedException("TODO");
+		}
+		
+		public void Commit(byte[] data)
+		{
+			throw new NotImplementedException("TODO");
+		}
+		
+		public Task CommitAsync(byte[] data)
+		{
+			throw new NotImplementedException("TODO");
+		}
+		
+		public void Dispose()
+		{
+			throw new NotImplementedException("TODO");
+		}
+	}
+}
