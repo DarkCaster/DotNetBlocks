56b4bd6efca5471a95760829c80789a0b300522e
DarkCaster <dark.caster@outlook.com>
2016-12-11 23:03:51 +0300

    Added FrameworkSafetyTests class, to perform some tests on internal classes to validate same behavior on different platforms (.NET\Mono\.NET core, etc)

---
+++ Tests/FrameworkSafetyTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Tests
+{
+	/// <summary>
+	/// Some tests for internal framework classes to test behavior in some specific cases.
+	/// </summary>
+	[TestFixture]
+	public class FrameworkSafetyTests
+	{
+	}
+}
