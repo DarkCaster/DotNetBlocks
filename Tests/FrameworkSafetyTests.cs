3ddbe34fb126f99fd2d9e7a2c50658687c9f9f11
DarkCaster <dark.caster@outlook.com>
2016-12-11 23:28:10 +0300

    FrameworkSafetyTests: added simple ConcurrentDictionary test case to check some corner cases later

--- Tests/FrameworkSafetyTests.cs
+++ Tests/FrameworkSafetyTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
 using NUnit.Framework;
 
 namespace Tests
@@ -9,5 +11,47 @@ namespace Tests
 	[TestFixture]
 	public class FrameworkSafetyTests
 	{
+		// https://gist.github.com/huysentruitw/f6f10cc1e9a10f2ef9bd5ab18f0b4f47
+		private static class ConcurrentDictionaryTestCase
+ 		{
+			private static ConcurrentDictionary<string, string> dict = new ConcurrentDictionary<string, string>();
+			private static int count=0;
+			public static void Execute()
+			{
+				var t1 = new Thread(() =>
+				dict.GetOrAdd("key1", x => {
+					Interlocked.Increment(ref count);
+					Thread.Sleep(TimeSpan.FromSeconds(1));
+					return x;
+				}));
+				var t2 = new Thread(() =>
+				dict.GetOrAdd("key2", x => {
+					Interlocked.Increment(ref count);
+					return x;
+				}));
+
+				var t3 = new Thread(() =>
+				dict.GetOrAdd("key1", x => {
+					Interlocked.Increment(ref count);
+					return x;
+				}));
+
+				t1.Start();
+				Thread.Sleep(500);
+				t2.Start();
+				t3.Start();
+				t1.Join();
+				t2.Join();
+				t3.Join();
+				if(count>2)
+					Assert.Fail(count.ToString());
+			}
+		}
+		
+		/*[Test()]
+		public void ConcurrentDictionary()
+		{
+			ConcurrentDictionaryTestCase.Execute();
+		}*/
 	}
 }
