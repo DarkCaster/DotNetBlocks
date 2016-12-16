c992aa5fbc79eac3d20284593a8fa75a2009cd75
DarkCaster <dark.caster@outlook.com>
2016-12-17 02:31:48 +0300

    AsyncRWLock: tests updated - added multi-threaded ReadLock locking test

--- Tests/AsyncRWLockTests.cs
+++ Tests/AsyncRWLockTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using DarkCaster.Async;
@@ -188,6 +189,65 @@ namespace Tests
 			rwLock.ExitReadLock();
 			AssertCounters(rwLock,0,0,false,0);
 			Task.Run( async () => await rwLock.EnterWriteLockAsync()).Wait();
+			AssertCounters(rwLock,0,0,true,0);
+			rwLock.ExitWriteLock();
+			AssertCounters(rwLock,0,0,false,0);
+		}
+		
+		[Test]
+		public void ReadLockMultithread()
+		{
+			var rwLock=new AsyncRWLock();
+			var reader1=new Task<bool>(()=>{
+			                           	if(rwLock.TryEnterReadLock())
+			                           		return false;
+			                           	rwLock.EnterReadLock();
+			                           	Thread.Sleep(250);
+			                           	rwLock.ExitReadLock();
+			                           	return true;
+			                     });
+			var reader2=new Task<bool>(()=>{
+			                           	if(rwLock.TryEnterReadLock())
+			                           		return false;
+			                           	rwLock.EnterReadLock();
+			                           	Thread.Sleep(250);
+			                           	rwLock.ExitReadLock();
+			                           	return true;
+			                     });
+			var reader3=new Task<bool>(()=>{
+			                           	if(rwLock.TryEnterReadLock())
+			                           		return false;
+			                           	rwLock.EnterReadLock();
+			                           	Thread.Sleep(250);
+			                           	rwLock.ExitReadLock();
+			                           	return true;
+			                     });
+			rwLock.EnterWriteLock();
+			AssertCounters(rwLock,0,0,true,0);
+			reader1.Start();
+			Thread.Sleep(100);
+			AssertCounters(rwLock,0,1,true,0);
+			reader2.Start();
+			Thread.Sleep(100);
+			AssertCounters(rwLock,0,2,true,0);
+			reader3.Start();
+			Thread.Sleep(100);
+			AssertCounters(rwLock,0,3,true,0);
+			rwLock.ExitWriteLock();
+			AssertCounters(rwLock,3,0,false,0);
+			Thread.Sleep(100);
+			AssertCounters(rwLock,3,0,false,0);
+			Assert.True(rwLock.TryEnterReadLock());
+			AssertCounters(rwLock,4,0,false,0);
+			rwLock.ExitReadLock();
+			AssertCounters(rwLock,3,0,false,0);
+			Assert.False(rwLock.TryEnterWriteLock());
+			AssertCounters(rwLock,3,0,false,0);
+			Assert.True(reader1.Result);
+			Assert.True(reader2.Result);
+			Assert.True(reader3.Result);
+			AssertCounters(rwLock,0,0,false,0);
+			Assert.True(rwLock.TryEnterWriteLock());
 			AssertCounters(rwLock,0,0,true,0);
 			rwLock.ExitWriteLock();
 			AssertCounters(rwLock,0,0,false,0);
