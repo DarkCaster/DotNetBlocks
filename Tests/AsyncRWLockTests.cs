cdd4ba427f9f2ffd61b37c57fc9b78291c593809
DarkCaster <dark.caster@outlook.com>
2016-12-16 15:28:46 +0300

    AsyncRWLock: added more tests

--- Tests/AsyncRWLockTests.cs
+++ Tests/AsyncRWLockTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using NUnit.Framework;
 using DarkCaster.Async;
 
@@ -65,6 +66,60 @@ namespace Tests
 			rwLock.ExitReadLock();
 			rwLock.ExitReadLock();
 			rwLock.EnterWriteLock();
+			rwLock.ExitWriteLock();
+		}
+		
+		[Test]
+		public void ReadLockAsync()
+		{
+			var rwLock=new AsyncRWLock();
+			Task.Run( async () => await rwLock.EnterReadLockAsync()).Wait();
+			rwLock.ExitReadLock();
+			Task.Run( async () => await rwLock.EnterReadLockAsync()).Wait();
+			Task.Run( async () => await rwLock.EnterReadLockAsync()).Wait();
+			Assert.False(rwLock.TryEnterWriteLock());
+			Assert.True(rwLock.TryEnterReadLock());
+			rwLock.ExitReadLock();
+			Assert.False(rwLock.TryEnterWriteLock());
+			rwLock.ExitReadLock();
+			Assert.False(rwLock.TryEnterWriteLock());
+			rwLock.ExitReadLock();
+		}
+		
+		[Test]
+		public void WriteLockAsync()
+		{
+			var rwLock=new AsyncRWLock();
+			Task.Run( async () => await rwLock.EnterWriteLockAsync()).Wait();
+			rwLock.ExitWriteLock();
+			
+			Task.Run( async () => await rwLock.EnterWriteLockAsync()).Wait();
+			Assert.False(rwLock.TryEnterWriteLock());
+			Assert.False(rwLock.TryEnterReadLock());
+			rwLock.ExitWriteLock();
+		}
+		
+		[Test]
+		public void LockSeqence1Async()
+		{
+			var rwLock=new AsyncRWLock();
+			Task.Run( async () => await rwLock.EnterWriteLockAsync()).Wait();
+			rwLock.ExitWriteLock();
+			Task.Run( async () => await rwLock.EnterReadLockAsync()).Wait();
+			Task.Run( async () => await rwLock.EnterReadLockAsync()).Wait();
+			rwLock.ExitReadLock();
+			rwLock.ExitReadLock();
+		}
+		
+		[Test]
+		public void LockSeqence2Async()
+		{
+			var rwLock=new AsyncRWLock();
+			Task.Run( async () => await rwLock.EnterReadLockAsync()).Wait();
+			Task.Run( async () => await rwLock.EnterReadLockAsync()).Wait();
+			rwLock.ExitReadLock();
+			rwLock.ExitReadLock();
+			Task.Run( async () => await rwLock.EnterWriteLockAsync()).Wait();
 			rwLock.ExitWriteLock();
 		}
 	}
