83c57e07baf3117e3a7028e279d180a5a0bec527
DarkCaster <dark.caster@outlook.com>
2017-05-11 02:42:30 +0300

    build-optional-deps.bat updated

--- build-optional-deps.bat
+++ build-optional-deps.bat
@@ -2,6 +2,8 @@
 REM Use random variable name, so calling another bat file will not spoil it.
 set aafc2aac361f=%~dp0
 
+del "%aafc2aac361f%\NuGet.Config"
+
 call "%aafc2aac361f%\External\DotNetBuildTools\prepare-and-build.bat" "%aafc2aac361f%\External"
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
 
