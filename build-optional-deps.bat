07b7b29df5a7e21e9895c756d45c888b132b4308
DarkCaster <dark.caster@outlook.com>
2017-05-11 03:00:35 +0300

    build-optional-deps.bat updated

--- build-optional-deps.bat
+++ build-optional-deps.bat
@@ -2,7 +2,10 @@
 REM Use random variable name, so calling another bat file will not spoil it.
 set aafc2aac361f=%~dp0
 
+echo "Removing custom NuGet.Config"
 del "%aafc2aac361f%\NuGet.Config"
+
+echo "Starting up build system (this may take some time)"
 
 call "%aafc2aac361f%\External\DotNetBuildTools\prepare-and-build.bat" "%aafc2aac361f%\External"
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
