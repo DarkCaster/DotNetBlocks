d491e6c3a22778f0a991a86b5ac7b5d23bc84bd5
DarkCaster <dark.caster@outlook.com>
2017-05-21 05:17:16 +0300

    DataTransfer: added ITunnelConfig and ITunnelConfigFactory interfaces

---
+++ CustomBlocks/DataTransfer/Abstracts/Config/ITunnelConfig.cs
@@ -0,0 +1,51 @@
+﻿// ITunnelConfig.cs
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2017 DarkCaster <dark.caster@outlook.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+using System;
+namespace DarkCaster.DataTransfer.Config
+{
+	/// <summary>
+	/// Interface for tunnel configuration.
+	/// May be used to pass some configuration parameters to internal INode\ITunnel components that used in data transfer process,
+	/// when creating new IEntryTunnel object.
+	/// All methods must be thread safe. Internal storage must be also thread safe and serializable.
+	/// Key name is not case sensitive.
+	/// </summary>
+	public interface ITunnelConfig
+	{
+		/// <summary>
+		/// Set config option value specified by key.
+		/// </summary>
+		/// <param name="key">Key.</param>
+		/// <param name="val">Value.</param>
+		void Set(string key, object val);
+
+		/// <summary>
+		/// Get config option value specified by key.
+		/// </summary>
+		/// <returns>Config value. Return null if there is no such key.</returns>
+		/// <param name="key">Key.</param>
+		object Get(string key);
+	}
+}
---
+++ CustomBlocks/DataTransfer/Abstracts/Config/ITunnelConfigFactory.cs
@@ -0,0 +1,50 @@
+﻿// ITunnelConfigFactory.cs
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2017 DarkCaster <dark.caster@outlook.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+using System;
+using DarkCaster.Serialization;
+namespace DarkCaster.DataTransfer.Config
+{
+	/// <summary>
+	/// Interface for factory class, that can create new ITunnelConfig storage 
+	/// and SerializationHelper object for ITunnelConfig that may be used to perform storage serialization
+	/// (for on-disk storage or transfering it over network).
+	/// </summary>
+	public interface ITunnelConfigFactory
+	{
+		/// <summary>
+		/// Creates new config storage object.
+		/// </summary>
+		/// <returns>New ITunnelConfig object. May (or may not) contain some default options.</returns>
+		ITunnelConfig CreateNew();
+		/// <summary>
+		/// Creates the serialization helper for ITunnelConfig object.
+		/// </summary>
+		/// <returns>
+		/// New serialization helper, that capable to perform serialization\deserialization of ITunnelConfig object
+		/// created with this factory object.
+		/// </returns>
+		ISerializationHelper<ITunnelConfig> CreateSerializationHelper();
+	}
+}
