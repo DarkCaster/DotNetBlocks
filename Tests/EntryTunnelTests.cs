24c4bf09aab2fa929d7f6d946d0cb20654723be8
DarkCaster <dark.caster@outlook.com>
2017-05-24 17:42:51 +0300

    Tests: EntryTunnel tests updated

--- Tests/EntryTunnelTests.cs
+++ Tests/EntryTunnelTests.cs
@@ -24,6 +24,7 @@
 //
 using System;
 using NUnit.Framework;
+using System.Threading;
 using DarkCaster.DataTransfer.Config;
 using DarkCaster.DataTransfer.Client;
 using Tests.Mocks;
@@ -36,23 +37,64 @@ namespace Tests
 		[Test]
 		public void NewNode()
 		{
-			var mockNode = new MockClientINode(0,0,0,0,10);
-			Assert.DoesNotThrow(()=>new EntryNode(mockNode));
+			var mockNode = new MockClientINode(0, 0, 0, 0, 10);
+			Assert.DoesNotThrow(() => new EntryNode(mockNode));
+		}
+
+		private int connectStateInvCount = 0;
+		private volatile TunnelState connectState = TunnelState.Init;
+		private volatile Exception connectEx = null;
+		private volatile bool connectDone = false;
+
+		private void NewTunnelConnectHandler (object sender, TunnelStateEventArgs args)
+		{
+			Interlocked.Increment(ref connectStateInvCount);
+			connectState = args.State;
+			connectEx = args.Ex;
+			connectDone = true;
 		}
 
 		[Test]
 		public void NewTunnel()
 		{
+			Interlocked.Exchange(ref connectStateInvCount, 0);
+			connectState = TunnelState.Init;
+			connectEx = null;
+			connectDone = false;
+
 			var mockNode = new MockClientINode(0, 0, 0, 0, 10);
 			var config = new MockITunnelConfigFactory().CreateNew();
 			IEntryNode node = null;
 			Assert.DoesNotThrow(() => { node = new EntryNode(mockNode); });
 			var tunnel = node.OpenTunnel(config);
+			tunnel.StateChangeEvent.Subscribe(NewTunnelConnectHandler);
 			Assert.DoesNotThrow(tunnel.Connect);
+
+			while(!connectDone) {}
+			Assert.AreEqual(TunnelState.Online, connectState);
+			if (connectEx != null)
+				throw connectEx;
+
+			connectEx = null;
+			connectDone = false;
+
 			Assert.DoesNotThrow(tunnel.Disconnect);
 			Assert.DoesNotThrow(tunnel.Disconnect); //multiple disconnect calls should be handled
+
+			while (!connectDone) { }
+			Assert.AreEqual(TunnelState.Offline, connectState);
+			if (connectEx != null)
+				throw connectEx;
+
 			Assert.DoesNotThrow(tunnel.Dispose);
 			Assert.DoesNotThrow(tunnel.Dispose); //multiple dispose calls should be handled
+
+			Assert.AreEqual(2, Interlocked.CompareExchange(ref connectStateInvCount, 0, 0));
+
+			var dbgTunnel = mockNode.LastTunnel;
+			Assert.NotNull(dbgTunnel);
+			Assert.AreEqual(1,dbgTunnel.DisconectCount);
+			Assert.AreEqual(1,dbgTunnel.DisposeCount);
 		}
 	}
 }
