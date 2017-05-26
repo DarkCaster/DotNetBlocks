77fe937886894f165b6df3d3d35a34da48c30c7d
DarkCaster <dark.caster@outlook.com>
2017-05-27 00:51:59 +0300

    DataTransfer: add ExitNode and ExitTunnel classes

---
+++ CustomBlocks/DataTransfer/ExitTunnel/ExitNode.cs
@@ -0,0 +1,110 @@
+﻿// ExitNode.cs
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2017 DarkCaster <dark.caster@outlook.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+using System;
+using System.Threading.Tasks;
+using DarkCaster.Async;
+using DarkCaster.Events;
+using DarkCaster.DataTransfer.Config;
+
+namespace DarkCaster.DataTransfer.Server
+{
+	public sealed class ExitNode : IExitNode
+	{
+		private INode upstreamNode = null;
+		private bool isDisposed = false;
+
+		private readonly AsyncRWLock upstreamLock = new AsyncRWLock();
+		private readonly ISafeEventCtrl<NewTunnelEventArgs> evCtl;
+		private readonly ISafeEvent<NewTunnelEventArgs> ev;
+
+		public ExitNode()
+		{
+#if DEBUG
+			evCtl = new SafeEventDbg<NewTunnelEventArgs>();
+			ev = (ISafeEvent<NewTunnelEventArgs>)evCtl;
+#else
+			evCtl = new SafeEvent<NewTunnelEventArgs>();
+			ev = (ISafeEvent<NewTunnelEventArgs>)evCtl;
+#endif
+		}
+
+		public ISafeEvent<NewTunnelEventArgs> IncomingConnectionEvent { get { return ev; } }
+
+		public void Init()
+		{
+			upstreamLock.EnterReadLock();
+			try { upstreamNode.Init(); }
+			finally { upstreamLock.ExitReadLock(); }
+		}
+
+		public void RegisterUpstream(INode upstream)
+		{
+			upstreamLock.EnterWriteLock();
+			upstreamNode = upstream;
+			upstreamLock.ExitWriteLock();
+		}
+
+		private void SpawnExitTunnel(ITunnelConfig config, ITunnel upstream)
+		{
+			var tunnel = new ExitTunnel(upstream);
+			Task.Run(() => evCtl.Raise(this, new NewTunnelEventArgs(tunnel, config)));
+		}
+
+		public async Task OpenTunnelAsync(ITunnelConfig config, ITunnel upstream)
+		{
+			await upstreamLock.EnterReadLockAsync();
+			try
+			{
+				if (isDisposed)
+				{
+					upstream.Dispose();
+					return;
+				}
+				SpawnExitTunnel(config, upstream);
+			}
+			finally
+			{
+				upstreamLock.ExitReadLock();
+			}
+		}
+
+		public void Dispose()
+		{
+			upstreamLock.EnterWriteLock();
+			try
+			{
+				if (isDisposed)
+					return;
+				isDisposed = true;
+				if (upstreamNode != null)
+					upstreamNode.Dispose();
+			}
+			finally
+			{
+				upstreamLock.ExitWriteLock();
+			}
+		}
+	}
+}
---
+++ CustomBlocks/DataTransfer/ExitTunnel/ExitTunnel.cs
@@ -0,0 +1,110 @@
+﻿// ExitTunnel.cs
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2017 DarkCaster <dark.caster@outlook.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DarkCaster.DataTransfer.Server
+{
+	public sealed class ExitTunnel : IExitTunnel
+	{
+		private readonly SemaphoreSlim readLock = new SemaphoreSlim(1, 1);
+		private readonly SemaphoreSlim writeLock = new SemaphoreSlim(1, 1);
+		private readonly ITunnel upstream;
+		private int isDisposed = 0;
+
+		public ExitTunnel(ITunnel upstream)
+		{
+			this.upstream = upstream;
+		}
+
+		public int ReadData(int sz, byte[] buffer, int offset = 0)
+		{
+			readLock.Wait();
+			try
+			{
+				return upstream.ReadData(sz, buffer, offset);
+			}
+			finally
+			{
+				readLock.Release();
+			}
+		}
+
+		public int WriteData(int sz, byte[] buffer, int offset = 0)
+		{
+			writeLock.Wait();
+			try
+			{
+				return upstream.WriteData(sz, buffer, offset);
+			}
+			finally
+			{
+				writeLock.Release();
+			}
+		}
+
+		public async Task<int> ReadDataAsync(int sz, byte[] buffer, int offset = 0)
+		{
+			await readLock.WaitAsync();
+			try
+			{
+				return await upstream.ReadDataAsync(sz, buffer, offset);
+			}
+			finally
+			{
+				readLock.Release();
+			}
+		}
+
+		public async Task<int> WriteDataAsync(int sz, byte[] buffer, int offset = 0)
+		{
+			await writeLock.WaitAsync();
+			try
+			{
+				return await upstream.WriteDataAsync(sz, buffer, offset);
+			}
+			finally
+			{
+				writeLock.Release();
+			}
+		}
+
+		public void Dispose()
+		{
+			if (Interlocked.CompareExchange(ref isDisposed, 1, 0) != 0)
+				return;
+			try
+			{
+				upstream.Dispose();
+			}
+			finally
+			{
+				readLock.Dispose();
+				writeLock.Dispose();
+			}
+		}
+	}
+}
