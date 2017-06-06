d74f17c6825bf88498f2b579f73abbc42aa3955f
DarkCaster <dark.caster@outlook.com>
2017-06-07 01:39:36 +0300

    CustomBlocks: FastLZ.cs - minor refactor

--- CustomBlocks/Compression/FastLZ.cs
+++ CustomBlocks/Compression/FastLZ.cs
@@ -56,12 +56,12 @@ namespace DarkCaster.Compression
 			}
 		}
 
-		public static int Compress(byte[] input, int ip, int length, byte[] output, int op, int level)
+		public static int Compress(byte[] input, int iPos, int iSz, byte[] output, int oPos, bool fastSpeed=true)
 		{
 			//TODO: input params check
-			int start = op;
-			int ip_bound = ip + length - 2;
-			int ip_limit = ip + length - 12;
+			int start = oPos;
+			int ip_bound = iPos + iSz - 2;
+			int ip_limit = iPos + iSz - 12;
 
 			int[] htab = new int[HASH_SIZE];
 			int hslot;
@@ -70,32 +70,32 @@ namespace DarkCaster.Compression
 			uint copy;
 
 			// sanity check
-			if (length < 4)
+			if (iSz < 4)
 			{
-				if (length > 0)
+				if (iSz > 0)
 				{
 					/* create literal copy only */
-					output[op++] = (byte)(length - 1);
+					output[oPos++] = (byte)(iSz - 1);
 					ip_bound++;
-					while (ip <= ip_bound)
-						output[op++] = input[ip++];
-					return length + 1;
+					while (iPos <= ip_bound)
+						output[oPos++] = input[iPos++];
+					return iSz + 1;
 				}
-				throw new ArgumentException("length param is not valid", nameof(length));
+				throw new ArgumentException("length param is not valid", nameof(iSz));
 			}
 
 			// initializes hash table
 			for (hslot = 0; hslot < HASH_SIZE; ++hslot)
-				htab[hslot] = ip;
+				htab[hslot] = iPos;
 
 			// we start with literal copy
 			copy = 2;
-			output[op++] = MAX_COPY - 1;
-			output[op++] = input[ip++];
-			output[op++] = input[ip++];
+			output[oPos++] = MAX_COPY - 1;
+			output[oPos++] = input[iPos++];
+			output[oPos++] = input[iPos++];
 
 			// main loop
-			while (ip < ip_limit)
+			while (iPos < ip_limit)
 			{
 				int refb;
 				uint distance;
@@ -104,22 +104,22 @@ namespace DarkCaster.Compression
 				uint len = 3;
 
 				// comparison starting-point
-				int anchor = ip;
+				int anchor = iPos;
 
-				if (level == 2)
+				if (!fastSpeed)
 				{
 					// check for a run
-					if (input[ip] == input[ip - 1] && FASTLZ_READU16(input, ip - 1) == FASTLZ_READU16(input, ip + 1))
+					if (input[iPos] == input[iPos - 1] && FASTLZ_READU16(input, iPos - 1) == FASTLZ_READU16(input, iPos + 1))
 					{
 						distance = 1;
-						ip += 3;
+						iPos += 3;
 						refb = anchor - 1 + 3;
 						goto match;
 					}
 				}
 
 				/* find potential match */
-				HASH_FUNCTION(ref hval, input, ip);
+				HASH_FUNCTION(ref hval, input, iPos);
 				hslot = (int)hval;
 				refb = htab[hval];
 
@@ -134,16 +134,16 @@ namespace DarkCaster.Compression
 
 
 
-				if (distance == 0 || (level < 2 && distance >= MAX_DISTANCE1) || (level == 2 && distance >= MAX_FARDISTANCE) ||
-						input[refb++] != input[ip++] || input[refb++] != input[ip++] || input[refb++] != input[ip++])
+				if (distance == 0 || (fastSpeed && distance >= MAX_DISTANCE1) || (!fastSpeed && distance >= MAX_FARDISTANCE) ||
+						input[refb++] != input[iPos++] || input[refb++] != input[iPos++] || input[refb++] != input[iPos++])
 					goto literal;
 
-				if (level == 2)
+				if (!fastSpeed)
 				{
 					// far, needs at least 5-byte match
 					if (distance >= MAX_DISTANCE2)
 					{
-						if (input[ip++] != input[refb++] || input[ip++] != input[refb++])
+						if (input[iPos++] != input[refb++] || input[iPos++] != input[refb++])
 							goto literal;
 						len += 2;
 					}
@@ -151,7 +151,7 @@ namespace DarkCaster.Compression
 			match:
 
 				// last matched byte
-				ip = (int)(anchor + len);
+				iPos = (int)(anchor + len);
 
 				// distance is biased
 				distance--;
@@ -159,62 +159,62 @@ namespace DarkCaster.Compression
 				if (distance == 0)
 				{
 					// zero distance means a run
-					byte x = input[ip - 1];
-					while (ip < ip_bound)
+					byte x = input[iPos - 1];
+					while (iPos < ip_bound)
 						if (input[refb++] != x)
 							break;
 						else
-							ip++;
+							iPos++;
 				}
 				else
 					for (;;)
 					{
 						// safe because the outer check against ip limit
-						if (input[refb++] != input[ip++]) break;
-						if (input[refb++] != input[ip++]) break;
-						if (input[refb++] != input[ip++]) break;
-						if (input[refb++] != input[ip++]) break;
-						if (input[refb++] != input[ip++]) break;
-						if (input[refb++] != input[ip++]) break;
-						if (input[refb++] != input[ip++]) break;
-						if (input[refb++] != input[ip++]) break;
-						while (ip < ip_bound)
-							if (input[refb++] != input[ip++]) break;
+						if (input[refb++] != input[iPos++]) break;
+						if (input[refb++] != input[iPos++]) break;
+						if (input[refb++] != input[iPos++]) break;
+						if (input[refb++] != input[iPos++]) break;
+						if (input[refb++] != input[iPos++]) break;
+						if (input[refb++] != input[iPos++]) break;
+						if (input[refb++] != input[iPos++]) break;
+						if (input[refb++] != input[iPos++]) break;
+						while (iPos < ip_bound)
+							if (input[refb++] != input[iPos++]) break;
 						break;
 					}
 
 				// if we have copied something, adjust the copy count
 				if (copy != 0)
 					// copy is biased, '0' means 1 byte copy
-					output[op - copy - 1] = (byte)(copy - 1);
+					output[oPos - copy - 1] = (byte)(copy - 1);
 				else
 					// back, to overwrite the copy count
-					op--;
+					oPos--;
 
 				// reset literal counter
 				copy = 0u;
 
 				// length is biased, '1' means a match of 3 bytes
-				ip -= 3;
-				len = (uint)(ip - anchor);
+				iPos -= 3;
+				len = (uint)(iPos - anchor);
 
 				// encode the match
-				if (level == 2)
+				if (!fastSpeed)
 				{
 					if (distance < MAX_DISTANCE2)
 					{
 						if (len < 7)
 						{
-							output[op++] = (byte)((len << 5) + (distance >> 8));
-							output[op++] = (byte)(distance & 255);
+							output[oPos++] = (byte)((len << 5) + (distance >> 8));
+							output[oPos++] = (byte)(distance & 255);
 						}
 						else
 						{
-							output[op++] = (byte)((7 << 5) + (distance >> 8));
+							output[oPos++] = (byte)((7 << 5) + (distance >> 8));
 							for (len -= 7; len >= 255; len -= 255)
-								output[op++] = 255;
-							output[op++] = (byte)len;
-							output[op++] = (byte)(distance & 255);
+								output[oPos++] = 255;
+							output[oPos++] = (byte)len;
+							output[oPos++] = (byte)(distance & 255);
 						}
 					}
 					else
@@ -223,21 +223,21 @@ namespace DarkCaster.Compression
 						if (len < 7)
 						{
 							distance -= MAX_DISTANCE2;
-							output[op++] = (byte)((len << 5) + 31);
-							output[op++] = 255;
-							output[op++] = (byte)(distance >> 8);
-							output[op++] = (byte)(distance & 255);
+							output[oPos++] = (byte)((len << 5) + 31);
+							output[oPos++] = 255;
+							output[oPos++] = (byte)(distance >> 8);
+							output[oPos++] = (byte)(distance & 255);
 						}
 						else
 						{
 							distance -= MAX_DISTANCE2;
-							output[op++] = (7 << 5) + 31;
+							output[oPos++] = (7 << 5) + 31;
 							for (len -= 7; len >= 255; len -= 255)
-								output[op++] = 255;
-							output[op++] = (byte)len;
-							output[op++] = 255;
-							output[op++] = (byte)(distance >> 8);
-							output[op++] = (byte)(distance & 255);
+								output[oPos++] = 255;
+							output[oPos++] = (byte)len;
+							output[oPos++] = 255;
+							output[oPos++] = (byte)(distance >> 8);
+							output[oPos++] = (byte)(distance & 255);
 						}
 					}
 
@@ -247,71 +247,71 @@ namespace DarkCaster.Compression
 					if (len > MAX_LEN - 2)
 						while (len > MAX_LEN - 2)
 						{
-							output[op++] = (byte)((7 << 5) + (distance >> 8));
-							output[op++] = MAX_LEN - 2 - 7 - 2;
-							output[op++] = (byte)(distance & 255);
+							output[oPos++] = (byte)((7 << 5) + (distance >> 8));
+							output[oPos++] = MAX_LEN - 2 - 7 - 2;
+							output[oPos++] = (byte)(distance & 255);
 							len -= MAX_LEN - 2;
 						}
 
 					if (len < 7)
 					{
-						output[op++] = (byte)((len << 5) + (distance >> 8));
-						output[op++] = (byte)(distance & 255);
+						output[oPos++] = (byte)((len << 5) + (distance >> 8));
+						output[oPos++] = (byte)(distance & 255);
 					}
 					else
 					{
-						output[op++] = (byte)((7 << 5) + (distance >> 8));
-						output[op++] = (byte)(len - 7);
-						output[op++] = (byte)(distance & 255);
+						output[oPos++] = (byte)((7 << 5) + (distance >> 8));
+						output[oPos++] = (byte)(len - 7);
+						output[oPos++] = (byte)(distance & 255);
 					}
 				}
 
 				// update the hash at match boundary
-				HASH_FUNCTION(ref hval, input, ip);
-				htab[hval] = ip++;
-				HASH_FUNCTION(ref hval, input, ip);
-				htab[hval] = ip++;
+				HASH_FUNCTION(ref hval, input, iPos);
+				htab[hval] = iPos++;
+				HASH_FUNCTION(ref hval, input, iPos);
+				htab[hval] = iPos++;
 
 				// assuming literal copy
-				output[op++] = MAX_COPY - 1;
+				output[oPos++] = MAX_COPY - 1;
 
 				continue;
 
 			literal:
-				output[op++] = input[anchor++];
-				ip = anchor;
+				output[oPos++] = input[anchor++];
+				iPos = anchor;
 				copy++;
 				if (copy == MAX_COPY)
 				{
 					copy = 0;
-					output[op++] = MAX_COPY - 1;
+					output[oPos++] = MAX_COPY - 1;
 				}
 			}
 
 			// left-over as literal copy
 			ip_bound++;
-			while (ip <= ip_bound)
+			while (iPos <= ip_bound)
 			{
-				output[op++] = input[ip++];
+				output[oPos++] = input[iPos++];
 				copy++;
 				if (copy == MAX_COPY)
 				{
 					copy = 0;
-					output[op++] = MAX_COPY - 1;
+					output[oPos++] = MAX_COPY - 1;
 				}
 			}
 
 			// if we have copied something, adjust the copy length
 			if (copy != 0)
-				output[op - copy - 1] = (byte)(copy - 1);
+				output[oPos - copy - 1] = (byte)(copy - 1);
 			else
-				op--;
+				oPos--;
 
-			if (level == 2)
+			if (!fastSpeed)
 				/* marker for fastlz2 */
 				output[start] |= (1 << 5);
 
-			return op - start;
+			return oPos - start;
 		}
 	}
 }
