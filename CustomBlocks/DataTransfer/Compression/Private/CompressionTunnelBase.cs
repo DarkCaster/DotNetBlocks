805ce74aaf5e1d434eebfe7a7266a55b5fa74b04
DarkCaster <dark.caster@outlook.com>
2017-07-12 03:46:07 +0300

    DataTransfer: CompressionTunnelBase updated

--- CustomBlocks/DataTransfer/Compression/Private/CompressionTunnelBase.cs
+++ CustomBlocks/DataTransfer/Compression/Private/CompressionTunnelBase.cs
@@ -38,9 +38,14 @@ namespace DarkCaster.DataTransfer.Private
 		private int uncReadBSZ;
 		private int readPhase;
 
-		private readonly int writeBlkSz;
+		private int compReadPos;
+		private int compReadMSZ;
+		private int compReadBSZ;
+
 		private readonly byte[] readBuff;
 		private readonly byte[] readBuffCompr;
+
+		private readonly byte[] writeBuffCompr;
 
 		protected CompressionTunnelBase(IBlockCompressor readCompr, IBlockCompressor writeCompr, ITunnelBase uplink)
 		{
@@ -53,6 +58,8 @@ namespace DarkCaster.DataTransfer.Private
 			uncReadPos = readCompr.MaxBlockSZ;
 			uncReadBSZ = 0;
 			readPhase = 0;
+
+			writeBuffCompr = new byte[writeCompr.GetOutBuffSZ(writeCompr.MaxBlockSZ)];
 		}
 
 		public async Task<int> ReadDataAsync(int sz, byte[] buffer, int offset = 0)
@@ -62,23 +69,82 @@ namespace DarkCaster.DataTransfer.Private
 			//do we need to read new compressed block
 			if(uncReadPos >= uncReadBSZ && readPhase == 0)
 				readPhase = 1;
-			//TODO: read compressed block preview
-			//TODO: decode compressed block metadata size
-			//TODO: read remaining compressed block metadata
-			//TODO: decode compressed block size
-			//TODO: read remaining compressed block data
-			//TODO: decompress data
-			//TODO: read decompressed data
-			throw new NotImplementedException("TODO");
+			//set-up counters for new compressed block
+			if(readPhase == 1)
+			{
+				compReadBSZ = 0;
+				compReadPos = 0;
+				readPhase++;
+			}
+			//read compressed block preview from uplink
+			if(readPhase == 2)
+			{
+				compReadPos += await uplink.ReadDataAsync(readCompr.MetadataPreviewSZ - compReadPos, readBuffCompr, compReadPos);
+				if(compReadPos < readCompr.MetadataPreviewSZ)
+					return 0;
+				readPhase++;
+			}
+			//decode compressed block metadata size
+			if(readPhase == 3)
+			{
+				compReadMSZ = readCompr.DecodeMetadataSZ(readBuffCompr, 0);
+				readPhase++;
+			}
+			//read remaining compressed block metadata from uplink
+			if(readPhase == 4)
+			{
+				compReadPos += await uplink.ReadDataAsync(compReadMSZ - compReadPos, readBuffCompr, compReadPos);
+				if(compReadPos < compReadMSZ)
+					return 0;
+				readPhase++;
+			}
+			//decode compressed block size
+			if(readPhase == 5)
+			{
+				compReadBSZ = readCompr.DecodeComprBlockSZ(readBuffCompr, 0);
+				if(compReadBSZ > readBuffCompr.Length)
+					throw new Exception("Too much data to decompress");
+				readPhase++;
+			}
+			//read remaining compressed block data from uplink
+			if(readPhase == 6)
+			{
+				compReadPos += await uplink.ReadDataAsync(compReadBSZ - compReadPos, readBuffCompr, compReadPos);
+				if(compReadPos < compReadBSZ)
+					return 0;
+				readPhase++;
+			}
+			//decompress data to temporary buffer
+			if(readPhase == 7)
+			{
+				uncReadBSZ = readCompr.Decompress(readBuffCompr, 0, readBuff, 0);
+				uncReadPos = 0;
+				readPhase = 0;
+			}
+			//read decompressed data from buffer
+			if(readPhase==0)
+			{
+				if(sz > uncReadBSZ - uncReadPos)
+					sz = uncReadBSZ - uncReadPos;
+				Buffer.BlockCopy(readBuff, uncReadPos, buffer, offset, sz);
+				uncReadPos += sz;
+			}
+			return sz;
 		}
 
 		public async Task<int> WriteDataAsync(int sz, byte[] buffer, int offset = 0)
 		{
 			if(sz == 0)
 				return 0;
-			//TODO: compress data
-			//TODO: write all compressed data to downstream
-			throw new NotImplementedException("TODO");
+			if(sz > writeCompr.MaxBlockSZ)
+				sz = writeCompr.MaxBlockSZ;
+			//compress data
+			var csz = writeCompr.Compress(buffer, sz, offset, writeBuffCompr, 0);
+			//write all compressed data to uplink
+			int wsz = 0;
+			while(wsz < csz)
+				wsz += await uplink.WriteDataAsync(csz - wsz, writeBuffCompr, wsz);
+			return sz;
 		}
 
 		public async Task DisconnectAsync()
