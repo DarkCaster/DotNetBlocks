ce2c32826ddee8a03e16daa2df5294c3cac55aae
DarkCaster <dark.caster@outlook.com>
2017-07-12 05:40:50 +0300

    DataTransfer: add CompressionServerTunnel class and CompressionServerNode class-draft

---
+++ CustomBlocks/DataTransfer/Compression/Server/CompressionServerNode.cs
@@ -0,0 +1,63 @@
+﻿// CompressionServerNode.cs
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2017 DarkCaster <dark.caster@outlook.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+using System;
+using System.Threading.Tasks;
+using DarkCaster.DataTransfer.Config;
+
+namespace DarkCaster.DataTransfer.Server.Compression
+{
+	public sealed class CompressionServerNode : INode
+	{
+		public Task InitAsync()
+		{
+			throw new NotImplementedException("TODO");
+		}
+
+		public void RegisterDownstream(INode downstream)
+		{
+			throw new NotImplementedException("TODO");
+		}
+
+		public Task OpenTunnelAsync(ITunnelConfig config, ITunnel upstream)
+		{
+			throw new NotImplementedException("TODO");
+		}
+
+		public Task NodeFailAsync(Exception ex)
+		{
+			throw new NotImplementedException("TODO");
+		}
+
+		public Task ShutdownAsync()
+		{
+			throw new NotImplementedException("TODO");
+		}
+
+		public void Dispose()
+		{
+			throw new NotImplementedException("TODO");
+		}
+	}
+}
