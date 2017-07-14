02d08d8de0acc10ef88fc73b2b57aa2f813f26e1
DarkCaster <dark.caster@outlook.com>
2017-07-14 18:55:47 +0300

    Tests: added CommonDataTransferTests class-draft - common tests for data transfer classes, added DT_CompressionNodeTests class-draft

---
+++ Tests/CommonDataTransferTests.cs
@@ -0,0 +1,72 @@
+﻿// CommonDataTransferTests.cs
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2017 DarkCaster <dark.caster@outlook.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+using System;
+using DarkCaster.Async;
+using DarkCaster.DataTransfer;
+using DarkCaster.DataTransfer.Config;
+using DarkCaster.Serialization.Binary;
+using Tests.Mocks.DataLoop;
+using NUnit.Framework;
+
+using SNode = DarkCaster.DataTransfer.Server.INode;
+using CNode = DarkCaster.DataTransfer.Client.INode;
+
+namespace Tests
+{
+	public static class CommonDataTransferTests
+	{
+		public static void NewConnection(ITunnelConfig clTunConfig, CNode clientNode, MockClientLoopNode clientLoopMock,  SNode serverNode, MockServerLoopNode serverLoopMock)
+		{
+			clientLoopMock.SetServerEntry(serverLoopMock);
+			var serverMockExit = new MockExitLoopNode(serverNode);
+			Assert.AreEqual(1, serverLoopMock.RegDsCount);
+			Assert.AreEqual(0, serverLoopMock.InitCount);
+			var runner = new AsyncRunner();
+			runner.ExecuteTask(serverMockExit.InitAsync);
+			Assert.AreEqual(1, serverLoopMock.InitCount);
+			Assert.IsNull(serverMockExit.IncomingConfig);
+			Assert.IsNull(serverMockExit.IncomingTunnel);
+			//create new connection
+			var clTun = runner.ExecuteTask(() => clientNode.OpenTunnelAsync(clTunConfig));
+			var svTun = serverMockExit.IncomingTunnel;
+			var svCfg = serverMockExit.IncomingConfig;
+			Assert.NotNull(svTun);
+			Assert.NotNull(svCfg);
+			Assert.AreEqual(1, serverLoopMock.NcCount);
+			Assert.AreEqual(0, serverLoopMock.ShutdownCount);
+			Assert.AreEqual(0, serverLoopMock.DisposeCount);
+			//simulate server shutdown
+			runner.ExecuteTask(serverMockExit.ShutdownAsync);
+			serverMockExit.Dispose();
+			Assert.AreEqual(1, serverLoopMock.ShutdownCount);
+			Assert.AreEqual(1, serverLoopMock.DisposeCount);
+			//try to open new connection again
+			Assert.Throws(typeof(Exception), () => runner.ExecuteTask(() => clientNode.OpenTunnelAsync(clTunConfig)));
+			Assert.IsNull(serverMockExit.IncomingConfig);
+			Assert.IsNull(serverMockExit.IncomingTunnel);
+		}
+
+	}
+}
---
+++ Tests/DT_CompressionNodeTests.cs
@@ -0,0 +1,62 @@
+﻿// CompressionDataTransferNodeTests.cs
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2017 DarkCaster <dark.caster@outlook.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+using System;
+using DarkCaster.Compression.FastLZ;
+using DarkCaster.Serialization.Binary;
+using DarkCaster.DataTransfer.Config;
+using DarkCaster.DataTransfer.Client.Compression;
+using DarkCaster.DataTransfer.Server.Compression;
+using Tests.Mocks.DataLoop;
+using NUnit.Framework;
+
+using SNode = DarkCaster.DataTransfer.Server.INode;
+using CNode = DarkCaster.DataTransfer.Client.INode;
+
+namespace Tests
+{
+	[TestFixture]
+	public class DT_CompressionNodeTests
+	{
+		[Test]
+		public void NewConnection()
+		{
+			var svConfig = new TunnelConfig();
+			//add mock parameters
+			svConfig.Set("mock_min_block_size", 64);
+			svConfig.Set("mock_max_block_size", 128);
+			svConfig.Set("mock_read_timeout", 5000);
+			svConfig.Set("mock_fail_prob", 0.0f);
+			svConfig.Set("mock_nofail_ops_count", int.MaxValue);
+			//add compression parameters
+			svConfig.Set("compr_max_block_size",16384);
+			var serverLoopMock = new MockServerLoopNode(svConfig, new TunnelConfigFactory(new BinarySerializationHelperFactory()));
+			var serverComprNode = new CompressionServerNode(svConfig, serverLoopMock, new FastLZBlockCompressorFactory());
+			var clConfig = new TunnelConfig();
+			var clientLoopMock = new MockClientLoopNode();
+			var clientComprNode = new CompressionClientNode(clientLoopMock, new FastLZBlockCompressorFactory());
+			CommonDataTransferTests.NewConnection(clConfig, clientComprNode, clientLoopMock, serverComprNode, serverLoopMock);
+		}
+	}
+}
